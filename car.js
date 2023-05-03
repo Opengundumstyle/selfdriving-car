5b3f24f66924bb28a255d1faadf8df7ff530d5c1
Zhile Lin <zhlin04@gmail.com>
2023-05-02 23:08:14 -0700

    reverse complete

--- car.js
+++ car.js
@@ -5,6 +5,20 @@ class Car{
          this.y= y;
          this.width = width;
          this.height = height;
+
+         this.controls = new Controls()
+
+     }
+
+     update(){
+          if(this.controls.forward){ 
+               this.y -= 2
+          }
+          if(this.controls.reverse){ 
+               this.y += 2
+          }
+         
+
      }
      
      draw(ctx){
@@ -20,4 +34,3 @@ class Car{
 
 }
 
-console.log("Car class loaded successfully");
--- main.js
+++ main.js
@@ -8,4 +8,10 @@ const ctx = canvas.getContext("2d");
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
 
-console.log("main.js loaded successfully");
+animate()
+
+function animate(){
+     car.update()
+     car.draw(ctx)
+     requestAnimationFrame(animate)
+}
